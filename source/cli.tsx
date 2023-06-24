e637debf38d9402abfa85b9628b5135cd6ef714c
LitoMore <litomore@gmail.com>
2023-06-25 04:03:52 +0800

    Farewell data.ai

--- source/cli.tsx
+++ source/cli.tsx
@@ -22,6 +22,7 @@ export function Home() {
 						location: 'Beijing',
 						title: 'Senior Front-End Engineer',
 						from: 'Mar, 2021',
+						to: 'Jun, 2023',
 					},
 					{
 						company: 'Beijing Ark Reading Technology Co., Ltd.',
